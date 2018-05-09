555642864212a357c1179beeb3e8a666ce59045a
Ted Anyansi <tanyansi@gmail.com>
2018-05-09 16:39:29 -0700

    imported Angular Router and packaged routes/components linkage into Router Module object that is imported, customized, and exported by a separate module that is imported into the main app module

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from '../about/about.component';
+import { LinksComponent } from '../links/links.component';
+import { ResumeComponent } from '../resume/resume.component';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'links',
+    component: LinksComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { LinksComponent } from './links/links.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    LinksComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

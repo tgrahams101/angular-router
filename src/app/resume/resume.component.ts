cfd975d26ab13a30fbbaba5c9ee68b681865f23d
Ted Anyansi <tanyansi@gmail.com>
2018-05-10 21:04:48 -0700

    modified resume component that it shows job based on current url param

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -9,10 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResumeComponent implements OnInit {
   jobIndex: number;
+  job: any;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.route.params);
+    console.log('RESUME ON IT');
+    this.route.params.forEach( (element) => {
+      console.log('CURRENT ELEMENT IN OBSERVABLE', element);
+      this.jobIndex = element.id;
+      this.job = JOBS.find( (job) => {
+        // tslint:disable-next-line:radix
+        return job.id === parseInt(element.id);
+      });
+      console.log('THE JOB INDEX', typeof this.jobIndex);
+      console.log('THE ACTUAL JOB', this.job);
+    });
   }
 
 }
